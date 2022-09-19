6f2750a95c27733207a94e45f4443aff28463a0c
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2022-09-19 20:25:09 +0530

    refactor:
    tried to place submit button in form but again placed in div

--- quiz.js
+++ quiz.js
@@ -1,8 +1,8 @@
 var formAnswers = document.querySelector("#form");
-var buttton = document.querySelector("button");
-var output = document.querySelector("output");
+var buttton = document.querySelector("#button");
+var output = document.querySelector("#output");
 
-const correctAnswers= ["90degree","right-angled","isosceles","second","90degree"];
+const correctAnswers= ["90degree","right-angled","isosceles","(1/2) x b x h","90degree"];
 
 
 function calculateScore(){
@@ -10,12 +10,13 @@ index=0;
 score=0;
 const data = new FormData(formAnswers);
  for(var value of data.values()){
+    console.log(value);
     if(value === correctAnswers[index]){
         score = score+1;
     }
     index= index+1;
  }
- output.innerText= "Your score is :-" + score;
+  output.innerText= "Your score is : " + score;
 }
 
 buttton.addEventListener("click",calculateScore);
